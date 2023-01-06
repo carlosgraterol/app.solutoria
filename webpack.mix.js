b4519a266cb7632edd8dd5a3afe7e5c2251fb68d
carlosgraterol <20089565castillocarlos@gmail.com>
2023-01-06 17:38:15 -0400

    Se agrega archivo readme.md

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .browserSync({
-        proxy: 'http://localhost/app.position/public',
+        proxy: 'http://localhost/app.solutoria/public',
         browser: ["chrome"]
     })
     .sourceMaps();
